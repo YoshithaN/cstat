d392dda9665ab9855a75150f306a9916e834c360
YoshithaN <yoshithanasi@gmail.com>
2026-09-17 22:55:08 +0530

    Create 26.py

---
+++ 26.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.array([10, 20, 30, 40, 50])
+
+mean = np.mean(a)
+variance = np.var(a)
+standard_deviation = np.std(a)
+
+print("Array:", a)
+print("Mean:", mean)
+print("Variance:", variance)
+print("Standard Deviation:", standard_deviation)
